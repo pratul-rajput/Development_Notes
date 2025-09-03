eaa91e8493fc2984454df86eb0baa0b1f18d3d97
Prabhu Narayan Singh <150592263+pratul-rajput@users.noreply.github.com>
2025-09-03 17:08:04 +0530

    Create gemini_free_api_demo.py

---
+++ gemini_free_api_demo.py
@@ -0,0 +1,11 @@
+#first create the api key from  google studio platform then pass your api key to the below snippet or keep in .env file and use it.
+
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key="AIzaSyDZ25QpOlSTrbKJ86Smu8ROYVZQeR2FbVQ")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain about Amrita Vishwa Vidyapeetham Mysore"
+)
+print(response.text)
